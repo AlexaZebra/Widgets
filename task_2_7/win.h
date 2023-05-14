63d2694520e909933af2192264cecb8935113395
Alexa <starovoytova.sasha05@gmail.com>
2023-05-14 14:42:14 +0700

    copy code

--- task_2_7/win.h
+++ task_2_7/win.h
@@ -1,21 +1,52 @@
 #ifndef WIN_H
 #define WIN_H
+//подключение необходимых библиотек
+#include <QtGui>
+#include <QWidget>    //Является базовым классом всех объектов пользовательского интерфейса.
+#include <QFrame>     //Базовый класс виджетов, которые могут иметь рамку.
+#include <QLabel>     //Обеспечивает отображение текста или изображения.
+#include <QLineEdit>  //Виджет представляет собой однострочный текстовый редактор.
+#include <QPushButton>//Виджет кнопка
+#include <QValidator> //Класс обеспечивает проверку вводимого текста.
 
-#include <QWidget>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class Win; }
+QT_BEGIN_NAMESPACE    //пространство имен для реализуемого виджета??????????
+namespace Ui { class Win; }//??????
 QT_END_NAMESPACE
 
-class Win : public QWidget
+class Win : public QWidget      // класс окна
 {
-    Q_OBJECT
+    Q_OBJECT                    // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+
+protected:
+ QTextCodec *codec;
+ QFrame *frame;                 // рамка
+ QLabel *inputLabel;            // метка ввода
+ QLineEdit *inputEdit;          // строчный редактор ввода
+ QLabel *outputLabel;           // метка вывода
+ QLineEdit *outputEdit;         // строчный редактор вывода
+ QPushButton *nextButton;       // кнопка Следующее
+ QPushButton *exitButton;       // кнопка Выход
 
 public:
     Win(QWidget *parent = nullptr);
-    ~Win();
+    ~Win(){};
+
+public slots:
+ void begin();                  // метод начальной настройки интерфейса
+ void calc();                   // метод реализации вычислений
+
 
 private:
     Ui::Win *ui;
 };
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+ StrValidator(QObject *parent):QValidator(parent){}
+ virtual State validate(QString &str,int &pos)const{
+    return Acceptable; // метод всегда принимает вводимую строку
+ }
+};
+
 #endif // WIN_H
